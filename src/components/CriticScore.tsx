94e942e71e1c419245d7b018751ab9a78250a8ea
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-04 20:46:00 +0300

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 90 ? "green" : score > 80 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
