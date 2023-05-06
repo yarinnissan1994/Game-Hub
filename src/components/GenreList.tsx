5e29a286782e9381cf3fa987e51f5fed0723bac0
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-06 13:53:19 +0300

    Refactor: fetching genres with react-query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -27,7 +27,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem paddingY="5px" key={genre.id}>
             <HStack>
               <Image
