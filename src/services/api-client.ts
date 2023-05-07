de7f7d18620a5df5461b9e2cf795216fbeddb442
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-08 01:09:36 +0300

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -23,6 +23,12 @@ class APIClient<T> {
   getAll = (config?: AxiosRequestConfig<T>) => {
     return axiosInstance.get(this.endpoint, config).then((res) => res.data);
   };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
