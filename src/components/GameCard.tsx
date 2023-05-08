089a3b056038f32e3b06e0cf69539dba0e92dd5d
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-08 09:40:16 +0300

    Improve game card link ux

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,21 +12,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image src={getCropImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent={"space-between"} marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-        </Heading>
-        <Emoji rating={game.rating_top} />
-      </CardBody>
-    </Card>
+    <Link to={"/games/" + game.slug}>
+      <Card>
+        <Image src={getCropImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <Emoji rating={game.rating_top} />
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
