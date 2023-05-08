5fd47fcff90b4e5166f05122689df609e015eb63
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-08 03:14:24 +0300

    Improving game detail page layout

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@chakra-ui/react";
 import useTrailers from "../hooks/useTrailers";
 
 interface Props {
@@ -14,7 +15,9 @@ const GameTrailer = ({ gameId }: Props) => {
   const first = data?.results[0];
 
   return first ? (
-    <video src={first.data[480]} poster={first.preview} controls />
+    <Box marginBottom={2}>
+      <video src={first.data[480]} poster={first.preview} controls />
+    </Box>
   ) : null;
 };
 
