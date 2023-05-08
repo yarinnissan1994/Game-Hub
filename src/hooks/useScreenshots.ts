34df01621672c15a2bb63eadacb7f1c2ddbcffe4
yarinnissan1994 <yarinnissan1994@gmail.com>
2023-05-08 03:08:16 +0300

    Build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient, { FetchResponse } from "../services/api-client";
+import { Screenshot } from "../entities/Screenshot";
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+
+  return useQuery<FetchResponse<Screenshot>, Error>({
+    queryKey: ["screenShots", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useScreenshots;
--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -4,7 +4,7 @@ import { Trailer } from "../entities/Trailer";
 
 const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
-  return useQuery<FetchResponse<Trailer>>({
+  return useQuery<FetchResponse<Trailer>, Error>({
     queryKey: ["trailers", gameId],
     queryFn: apiClient.getAll,
   });
